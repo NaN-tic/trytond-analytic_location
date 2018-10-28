d755dfccab91603a018abf2db33a2c19d0f69fba
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-28 11:43:32 +0100

    Migrate to python 3

--- location.py
+++ location.py
@@ -15,8 +15,7 @@ from trytond.modules.analytic_account import AnalyticMixin
 __all__ = ['Location', 'LocationCompany', 'AnalyticAccountEntry']
 
 
-class Location:
-    __metaclass__ = PoolMeta
+class Location(metaclass=PoolMeta):
     __name__ = 'stock.location'
     companies = fields.One2Many('stock.location.company', 'location',
         'Configuration by company')
@@ -129,8 +128,7 @@ class LocationCompany(AnalyticMixin, ModelSQL, ModelView):
         return Transaction().context.get('company')
 
 
-class AnalyticAccountEntry:
-    __metaclass__ = PoolMeta
+class AnalyticAccountEntry(metaclass=PoolMeta):
     __name__ = 'analytic.account.entry'
 
     @classmethod
